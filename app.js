175972c7abe8451bb4ba56d90760f84febe998cc
Matt Lyons <matt@matt-lyons.net>
2015-06-04 13:02:33 -0500

    Organized

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); //Serve files from public
+app.use('/javascripts/game', express.static(path.join(__dirname, 'shared'))); //Serve files from shared into public javascript folder
 
 app.use('/', routes);
 app.use('/play', play);
