df2b17d526915c5aa74851a413e3392fc305bf7f
Matt Lyons <matt@matt-lyons.net>
2015-06-29 21:03:42 -0500

    Implemented High Volume Tolerance for WebServer

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var toobusy = require('toobusy-js');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -12,74 +13,116 @@ var play = require('./routes/play');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+setupJade();
+setupMorgan();
+setupTooBusy();
+setupPaths();
+setupErrorHandlers()
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public'))); //Serve files from public
-app.use('/javascripts/game', express.static(__dirname + '/shared')); //Serve files from shared into public javascript folder
-app.use('/javascripts/game', express.static(__dirname + '/client')); //Serve files from client into public javascript folder
+function setupJade() {
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+}
 
-app.use('/', routes);
-app.use('/play', play);
+function setupMorgan() {
+  if (app.get('env') === 'development') {
+    app.use(logger('dev')); //Logs Webserver Requests
+  } else {
+    app.use(logger('common', {
+      skip: function (req, res) {
+        return res.statusCode < 400
+      }
+    }));
+  }
+}
+function setupTooBusy() {
+  //Block requests when server is overloaded
+  app.use(function (req, res, next) {
+    if (toobusy()) {
+      var err = new Error("TooBusy");
+      err.status = 503;
+      next(err);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+    } else {
+      next();
+    }
+  });
+}
+
+function setupErrorHandlers(){
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  // error handlers
+
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      this.renderError(err, res, true);
+    });
+  }
+
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+    this.renderError(err, res, false);
+  });
+
+  this.renderError=function renderError(err,res, devEnv) {
+
+    res.status(err.status || 500);
+    if(err.message==='TooBusy'){
+      res.render('tooBusy', {
+        error: err
+      });
+    } else {
+      res.render('error', {
+        message: err.message,
+        error: devEnv ? err : {} //Display stacktrace if development is the environment
+      });
+    }
+  }
+}
+
+function setupPaths() {
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(__dirname + '/public/favicon.ico'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public'))); //Serve files from public
+  app.use('/javascripts/game', express.static(__dirname + '/shared')); //Serve files from shared into public javascript folder
+  app.use('/javascripts/game', express.static(__dirname + '/client')); //Serve files from client into public javascript folder
+
+  app.use('/', routes);
+  app.use('/play', play);
+}
 
 //Manage shutdown gracefully
 var shutdown=function() {
   debug('Received shutdown request. Server is shutting down...');
   app.sio.close();
-  debug('Socket.IO has shutdown.');
-  app.server.close(function () {
-    debug('Express has shutdown.');
-    process.exit();
-  });
+  debug('Socket.IO Shutdown.');
+  app.server.close();
+  debug('Server Shutdown');
+  toobusy.shutdown();
+  debug('TooBusy Shutdown');
+
+  process.exit();
+
   setTimeout(function () {
     error('Could not shutdown express in 30 seconds. Forcing shutdown.');
     process.exit();
-  }, 30 * 1000);//If cannot shutdown after 30 seconds forcefully shutdown express
+  }, 30 * 1000);//If cannot shutdown after 30 seconds forcefully shutdown
 };
 
 //Gracefully Shutdown upon receiving these signals
-//process.on('SIGTERM', shutdown);
-//process.on('SIGINT', shutdown);
-//process.on('SIGHUP', shutdown);
-//process.on('SIGQUIT', shutdown);
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+process.on('SIGINT', shutdown);
 
 //var repl = require("repl");
 //r = repl.start("node> ");
